35a777ecea5c4470310db1b176298b3e5fb525ed
mromasanta <109672900+mromasanta@users.noreply.github.com>
2023-01-27 16:19:53 +0800

    deploy

---
+++ js/about-legacy.185d5356.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportfolio"]=self["webpackChunkportfolio"]||[]).push([[443],{9030:function(t,e,n){n.r(e),n.d(e,{default:function(){return i}});var u=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},l=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],r=n(1001),s={},a=(0,r.Z)(s,u,l,!1,null,null,null),i=a.exports}}]);
+//# sourceMappingURL=about-legacy.185d5356.js.map
